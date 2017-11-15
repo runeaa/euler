ea316e15d9dd1feaed3f6cd281e263ef8846bb88
runenaa <r.aasvestad@live.com>
2017-11-15 23:36:09 +0100

    Første problemet på project euler i kotlin

---
+++ src/no/runsoft/problems/FirstProblem.kt
@@ -0,0 +1,13 @@
+package no.runsoft.problem.one
+
+fun main(args: Array<String>) {
+    var sum = 0
+    var x = 0
+    while (x < 1000){
+        if (x % 3 == 0 || x % 5 == 0) {
+            sum += x
+        }
+        x++
+    }
+    println(sum)
+}
